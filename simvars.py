8a32bb76b6e54ce4208563290ad005a1847434d2
Jonah Silverman <jonahsilverman11@gmail.com>
2021-02-15 17:44:21 -0500

    Added Files

---
+++ simvars.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 20 17:47:10 2020
+
+@author: jonah
+"""
+
+posplace = 99 
+pospos = 90
+length = 50
+numgens = 40
+uninfec = 0
+infec = 1
+rest = 2
+ci1 = .1 
+ci2 = .2
+ci3 = .3
+ci4 = .4
+ci5 = .5
+ci6 = .6
+ci7 = .7
+ci8 = .8
+startinginfec =.05
+
+it1 = -2
+it2 = -3
+it3 = -4
+it4 = -5
+it5 = -6
+it6 = -7
+
+
+incubr1 = .2 
+incubr2 = .4
+incubr3 = .6
+incubr4 = .8
+incubr5 = 1.0
+
+
+rr1 = 0.2
+rr2 = 0.4
+rr3 = 0.6
+rr4 = 0.8
+rr5 = 1.0
+
+rt1 = -2
+rt2 = -3
+rt3 = -4
+rt4 = -5
+rt5 = -6
+
+start = 0
+gencount = 0
+
+data = dict()
